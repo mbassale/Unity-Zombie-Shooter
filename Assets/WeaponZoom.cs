7c349d5b2ba09c7a198626c4bf72d1b648cc099e
Marco Bassaletti <bassaletti@gmail.com>
2019-09-03 17:09:47 -0400

    Weapon Zoom: support zooming with animation.

---
+++ Assets/WeaponZoom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] private float zoomedOutFOV = 60f;
+    [SerializeField] private float zoomedInFOV = 20f;
+    private Camera fpsCamera;
+    private bool zoomedIn = false;
+    private float zoomValue;
+
+    private void Start()
+    {
+        fpsCamera = Camera.main;
+        zoomValue = zoomedOutFOV;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            zoomedIn = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            zoomedIn = false;
+        }
+
+        if (zoomedIn)
+        {
+            if (zoomValue > zoomedInFOV)
+            {
+                zoomValue = Mathf.LerpUnclamped(zoomValue, zoomedInFOV, Time.deltaTime * 10f);
+            }
+            fpsCamera.fieldOfView = zoomValue;
+        }
+        else
+        {
+            if (zoomValue < zoomedOutFOV)
+            {
+                zoomValue = Mathf.LerpUnclamped(zoomValue, zoomedOutFOV, Time.deltaTime * 10f);
+            }
+            fpsCamera.fieldOfView = zoomValue;
+        }
+    }
+}
