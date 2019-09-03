00525c9a839963b54073da3049bd804a0f865a78
Marco Bassaletti <bassaletti@gmail.com>
2019-09-03 17:18:19 -0400

    Weapon Zoom: increased mouse sensitivity when zooming in.

--- Assets/WeaponZoom.cs
+++ Assets/WeaponZoom.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] private float zoomedOutFOV = 60f;
     [SerializeField] private float zoomedInFOV = 20f;
+    [SerializeField] private float zoomOutSensitivity = 2f;
+    [SerializeField] private float zoomInSensitivity = 0.5f;
     private Camera fpsCamera;
     private bool zoomedIn = false;
     private float zoomValue;
+    private RigidbodyFirstPersonController fpsController;
 
     private void Start()
     {
         fpsCamera = Camera.main;
         zoomValue = zoomedOutFOV;
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
     }
 
     private void Update()
@@ -33,6 +38,8 @@ public class WeaponZoom : MonoBehaviour
                 zoomValue = Mathf.LerpUnclamped(zoomValue, zoomedInFOV, Time.deltaTime * 10f);
             }
             fpsCamera.fieldOfView = zoomValue;
+            fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomInSensitivity;
         }
         else
         {
@@ -41,6 +48,8 @@ public class WeaponZoom : MonoBehaviour
                 zoomValue = Mathf.LerpUnclamped(zoomValue, zoomedOutFOV, Time.deltaTime * 10f);
             }
             fpsCamera.fieldOfView = zoomValue;
+            fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
         }
     }
 }
