0d324a01137d276ac120c171dec16040567566de
Marco Bassaletti <bassaletti@gmail.com>
2019-09-04 22:27:44 -0400

    Created basic Ammo Pickup prefab.

---
+++ Assets/AmmoPickup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Picking up");
+        }
+    }
+}
